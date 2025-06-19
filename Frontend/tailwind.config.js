deda5238afb9178dc7cc129da154076a569d0fe7
Mohit Singh <golchigeek@gmail.com>
2025-06-20 01:20:33 +0530

    Add Folder Structure

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        terminal: ["Share Tech Mono", "monospace"],
+      },
+      animation: {
+        glitch: "glitch 1s infinite",
+      },
+    },
+  },
+  plugins: [],
+};
